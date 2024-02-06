3963a53cacf02fd258384c9889bdea0d42d32565
hsntareq <hsntareq@gmail.com>
2024-02-06 19:21:55 +0600

    Featured design done except responsiveness

--- js/script.js
+++ js/script.js
@@ -2,6 +2,40 @@ console.log("script loaded");
 var mobileToggler = document.querySelectorAll(".mobile-toggler");
 mobileToggler.forEach((element) => {
   element.addEventListener("click", () => {
-    element.parentElement.parentElement.classList.toggle("open");
+    console.log("newerw");
+    removeGrandparentClass(element, "open", "nav");
   });
 });
+
+// Get the header element
+var header = document.querySelector("header");
+
+// Get the offset position of the header
+var stickyOffset = header.offsetTop + 50;
+
+// Listen for the scroll event and add or remove the sticky class
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > stickyOffset) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
+
+function findNavGrandparent(element) {
+  var parent = element.parentNode;
+
+  while (parent && parent.tagName !== "NAV") {
+    parent = parent.parentNode;
+  }
+
+  return parent;
+}
+
+function removeGrandparentClass(button, targetClass) {
+  var navGrandparent = findNavGrandparent(button);
+
+  if (navGrandparent) {
+    navGrandparent.classList.toggle(targetClass);
+  }
+}
